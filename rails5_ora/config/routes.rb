ea25c692406de136476b75343e10cd62ca34021f
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2019-04-08 18:00:51 +0200

    Text commit

--- rails5_ora/config/routes.rb
+++ rails5_ora/config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   post 'oracle_test/bind_array_in'
   post 'oracle_test/bind_freeform'
   get 'oracle_test/bind'
+
   get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #
