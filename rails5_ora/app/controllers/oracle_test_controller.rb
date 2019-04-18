6c7376af89df34d44fa60531a14bf01f5fc4f1fd
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2019-04-18 11:14:41 +0200

    Bind checks added for find_by_sql and exec_query

--- rails5_ora/app/controllers/oracle_test_controller.rb
+++ rails5_ora/app/controllers/oracle_test_controller.rb
@@ -27,13 +27,44 @@ class OracleTestController < ApplicationController
   end
 
   def bind_xy_result
-    @ts = DbaTablespace.where(@sql_param).all   # Load structure
-    @length = @ts.length       # Ensure execution of Select
-    @ts = DbaTablespace.where(@sql_param).all
-    @length = @ts.length       # Ensure execution of Select
-    @plans = ActiveRecord::Base.connection.exec_query("SELECT * FROM table(DBMS_XPLAN.DISPLAY_CURSOR)")
-
-    render 'oracle_test/bind_xy_result'
+    ts = DbaTablespace.where(@sql_param).all   # Load structure
+    @length = ts.length       # Ensure execution of Select
+    @result = DbaTablespace.where(@sql_param).all
+    @length = @result.length       # Ensure execution of Select
+    render_result
   end
 
+  def find_by_sql_array
+    @runmode = 'find_by_sql Array'
+    @sql_param = ["SELECT * FROM DBA_Tablespaces WHERE TableSpace_Name = ?", 'SYSTEM']
+
+    ts = DbaTablespace.find_by_sql(@sql_param)
+    @length = ts.length       # Ensure execution of Select
+    @result = DbaTablespace.find_by_sql(@sql_param)
+    @length = @result.length       # Ensure execution of Select
+    render_result
+  end
+
+  def exec_query_array
+    @runmode = 'exec_query Array'
+    @sql = "SELECT * FROM DBA_Tablespaces WHERE TableSpace_Name = :name"
+
+    # Syntax starting with Rails 4.2
+    # @sql_param = [ ActiveRecord::ConnectionAdapters::Column.new(':name', nil, ActiveRecord::Type::Value.new),'SYSTEM' ]
+    # Syntax starting with Rails 5
+    @sql_param = [ActiveRecord::Relation::QueryAttribute.new(':name', 'SYSTEM', ActiveRecord::Type::Value.new)]
+
+    ts = ActiveRecord::Base.connection.exec_query(@sql, 'MySQL', @sql_param)
+    @length = ts.length       # Ensure execution of Select
+    @result = ActiveRecord::Base.connection.exec_query(@sql, 'MySQL', @sql_param)
+    @length = @result.length       # Ensure execution of Select
+    render_result
+  end
+
+
+private
+  def render_result
+    @plans = ActiveRecord::Base.connection.exec_query("SELECT * FROM table(DBMS_XPLAN.DISPLAY_CURSOR)")
+    render 'oracle_test/bind_xy_result'
+  end
 end
--- rails5_ora/config/routes.rb
+++ rails5_ora/config/routes.rb
@@ -1,12 +1,23 @@
 Rails.application.routes.draw do
+  Rails.logger.info "routes.rb: Setting routes for every controller action"
+  WelcomeController.routing_actions("#{__dir__}/../app/controllers").each do |r|
+    # puts "set route for #{r[:controller]}/#{r[:action]}"
+    get  "#{r[:controller]}/#{r[:action]}"
+    post  "#{r[:controller]}/#{r[:action]}"
+  end
+
+
+=begin
   get 'oracle_test/bind_hash'
   get 'oracle_test/bind_array'
   post 'oracle_test/bind_hash'
   post 'oracle_test/bind_array'
   post 'oracle_test/bind_array_in'
   post 'oracle_test/bind_freeform'
+  post 'oracle_test/find_by_sql_array'
+  post 'oracle_test/find_by_sql_hash'
   get 'oracle_test/bind'
-
+=end
   get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #
